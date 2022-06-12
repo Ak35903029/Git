2145019b004e22f2ac900012453cc781f824cbc7
Abdussamad Khan <Abdussamad Khan@khans-desktop29>
2022-06-12 19:40:06 +0530

    commit first

---
+++ king/src/king/Shit.java
@@ -0,0 +1,15 @@
+package king;
+
+public class Shit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi there");
+		int x,b;
+		x = 23;
+		b = 34;
+		System.out.println(x*b);
+
+	}
+
+}
